94305bd0ba5a263e947acec20b1c800327a8c19d
Varcrow <eminns28@gmail.com>
2025-12-03 21:39:02 -0500

    fix filter instantiation

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 // For censoring thy gamer words
-const filter = Filter();
+const filter = new Filter();
 
 app.use(cors());
 app.use(express.json());
